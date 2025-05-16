1b15c388cf8246e24b833e7a7f03c0a537400355
shadow3aaa <74781933+shadow3aaa@users.noreply.github.com>
2025-05-16 20:09:41 +0800

    更新依赖项版本以支持最新功能和修复

--- setup.py
+++ setup.py
@@ -16,16 +16,16 @@ setuptools.setup(
     packages=setuptools.find_packages("src"),
     python_requires=">=3.10",
     install_requires=[
-        "torch==2.5.1",
-        "transformers==4.45.2",
-        "vllm==0.6.6.post1",
-        "openai==1.58.1",
+        "openai==1.78.1",
+        "vllm==0.8.5.post1",
         "gritlm==1.0.2",
+        "torch==2.6.0",
+        "transformers==4.51.3",
         "networkx==3.4.2",
-        "python_igraph==0.11.8",
-        "tiktoken==0.7.0",
         "pydantic==2.10.4",
-        "tenacity==8.5.0",
+        "python_igraph==0.11.8",
+        "tenacity==9.1.2",
+        "tiktoken==0.9.0",
         "einops", # No version specified
         "tqdm", # No version specified
     ]
